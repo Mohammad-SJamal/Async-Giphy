c35b86faa0be03e1b15d28e98c66d93b32bae65b
Mohammad-SJamal <mohammad.jamal@verizon.com>
2025-05-12 13:00:58 -0500

    finished choosing random url

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-//require('dotenv').config();
-import "dotenv/config.js";
+require('dotenv').config();
+//import "dotenv/config.js";
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
-const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=the+rock&limit=25&offset=0&rating=g&lang=en`
 
 async function getImage(query){
-    const response = await fetch(query);
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`
+    const response = await fetch(endpoint);
     const data = await response.json();
-    console.log(data);
-    return data;
+    let images = [];
+    for (let i of data["data"]){
+        images.push(i.url);
+    }
+    //return data["data"][0].url;
+    return images[Math.floor(Math.random() * images.length)]; //returns a random url from the data json
 }
 
-await getImage(endpoint);
+
+async function helperFunction(query){ //so we can call getImage with await
+    const url = await getImage(query);
+    console.log(url);
+}
+
+helperFunction("dogs");
 
